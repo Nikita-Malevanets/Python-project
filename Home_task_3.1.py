b923acbf1343c0c79be2ce244f75f679113ae468
nikita-malevanec <nikitamalevanec@yahoo.com>
2025-06-22 22:31:28 +0200

    create new file

---
+++ Home_task_3.1.py
@@ -0,0 +1,15 @@
+number1 = float(input("Enter number#1  "))
+number2 = float(input("Enter number#2  "))
+operation = input("Enter arithmetic operation (+, -, *, / ): ")
+
+if operation == '-':
+    print ( "result: ", number1 - number2)
+if operation == '+':
+    print ( "result: ", number1 + number2)
+if operation == '*':
+    print ( "result: ", number1 * number2)
+if operation == '/':
+    if number2 == 0:
+        print ( "ERROR! You cannot divide by 0.")
+    else:
+        print ( "result: ", number1 / number2)
