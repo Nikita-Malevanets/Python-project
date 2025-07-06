ef0bafe4282682e86e41ffbb96659d606005306e
nikita-malevanec <nikitamalevanec@yahoo.com>
2025-07-06 21:56:21 +0200

    Fixed errors.

--- Home_task_7.4.py
+++ Home_task_7.4.py
@@ -3,16 +3,19 @@
 def common_elements():
     first_list = []
     second_list = []
-    for item in range(100):
-        first_list.append(item*3)
-        second_list.append(item*5)
+    for item in range(0,101):
+        if item % 3 == 0:
+            first_list.append(item)
+        if item % 5 == 0:
+            second_list.append(item)
 
     first_list = set(first_list)
     second_list = set(second_list)
     same_elements = first_list.intersection(second_list)
     return same_elements
 
-print(common_elements())
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print("ОК")
 
 
 
