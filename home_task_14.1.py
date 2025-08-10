67b7243e993ccb74dad423540959d64a9fba44ff
nikita-malevanec <nikitamalevanec@yahoo.com>
2025-08-10 18:03:49 +0200

    new home task 14.1

--- home_task_14.1.py
+++ home_task_14.1.py
@@ -1,6 +1,3 @@
-from re import fullmatch
-
-
 class Human:
 
     def __init__(self, gender, age, first_name, last_name):
@@ -18,6 +15,13 @@ class Student(Human):
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
+
+    def __eq__(self, other):
+        if not isinstance(other, Student):
+            return False
+        return str(self) == str(other)
+    def __hash__(self):
+        return hash(str(self))
 
     def __str__(self):
         return f'{super().__str__()} {self.record_book}'
